1c52b29396b9f5764874b9bed1189ef3ae27d845
Steffi Stumpos <stumpos@fb.com>
2022-08-19 15:42:35 -0600

    wip sizing

--- src/beanmachine/ppl/compiler/tests/size_assessment_test.py
+++ src/beanmachine/ppl/compiler/tests/size_assessment_test.py
@@ -17,27 +17,27 @@ class SizeAssessmentTests(unittest.TestCase):
     def test_matrix_mult(self):
         bmg = BMGraphBuilder()
         assessor = SizeAssessment(Sizer())
-        probs = bmg.add_real_matrix(torch.tensor([[0.75, 0.25], [0.125, 0.875]]))
+        probs = bmg.add_real_matrix(torch.tensor([[0.5, 0.125, 0.125], [0.0625, 0.0625, 0.875]]))
         tensor_elements = []
         for row in range(0, 2):
             row_node = bmg.add_natural(row)
             row_prob = bmg.add_column_index(probs, row_node)
-            for column in range(0, 2):
+            for column in range(0, 3):
                 col_index = bmg.add_natural(column)
                 prob = bmg.add_vector_index(row_prob, col_index)
                 bernoulli = bmg.add_bernoulli(prob)
                 sample = bmg.add_sample(bernoulli)
                 tensor_elements.append(sample)
-        matrix2by2 = bmg.add_tensor(Size([2, 2]), *tensor_elements)
+        matrix2by3_rhs = bmg.add_tensor(Size([2, 3]), *tensor_elements)
         # invalid
-        matrix3by3 = bmg.add_real_matrix(
-            torch.tensor([[0.21, 0.27, 0.3], [0.5, 0.6, 0.1], [0.8, 0.6, 0.9]])
+        matrix2by3 = bmg.add_real_matrix(
+            torch.tensor([[0.21, 0.27, 0.3], [0.5, 0.6, 0.1]])
         )
         matrix1by3 = bmg.add_real_matrix(torch.tensor([[0.1, 0.2, 0.3]]))
-        matrix2 = bmg.add_real_matrix(torch.tensor([0.1, 0.2]))
+        matrix3 = bmg.add_real_matrix(torch.tensor([0.1, 0.2, 0.9]))
         scalar = bmg.add_real(4.5)
 
-        mm_invalid = bmg.add_matrix_multiplication(matrix2by2, matrix3by3)
+        mm_invalid = bmg.add_matrix_multiplication(matrix2by3_rhs, matrix2by3)
         error_size_mismatch = assessor.size_error(mm_invalid, bmg)
         self.assertIsInstance(error_size_mismatch, BadMatrixMultiplication)
         expectation = """
@@ -46,13 +46,14 @@ The dimensions of the operands are 2x2 and 3x3.
         """
         self.assertEqual(expectation.strip(), error_size_mismatch.__str__().strip())
 
-        broadcast_not_supported_yet = bmg.add_matrix_multiplication(matrix2by2, matrix1by3)
+        broadcast_not_supported_yet = bmg.add_matrix_multiplication(matrix2by3_rhs, matrix1by3)
+        error_broadcast_not_supported_yet = assessor.size_error(broadcast_not_supported_yet, bmg)
         expectation = """"""
 
         self.assertEqual(expectation.strip(), broadcast_not_supported_yet.__str__().strip())
         errors = [
-            assessor.size_error(bmg.add_matrix_multiplication(matrix2by2, mm), bmg)
-            for mm in [matrix2, scalar]
+            assessor.size_error(bmg.add_matrix_multiplication(matrix2by3_rhs, mm), bmg)
+            for mm in [matrix3, scalar]
         ]
         for error in errors:
             self.assertIsNone(error)
